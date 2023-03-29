6311d374ffec478cfb1626a49610928627d6f165
sindhukeesara <87634015+sindhukeesara@users.noreply.github.com>
2023-03-29 23:08:39 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("java file");  
+         System.out.println("Modified java file");  
     }  
 }  
